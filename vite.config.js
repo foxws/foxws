37a81f49d20e25c622fc2481e9167af88d52b499
francoism90 <francoism90@users.noreply.github.com>
2024-04-11 16:07:02 +0200

    Fix Vite

--- vite.config.js
+++ vite.config.js
@@ -1,71 +1,90 @@
 import { defineConfig } from 'vite';
+import { readFileSync } from 'fs';
 import { fileURLToPath, URL } from 'url';
 import { VitePWA } from 'vite-plugin-pwa';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
-export default defineConfig(() => ({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./', import.meta.url)),
-      '~': fileURLToPath(new URL('./node_modules', import.meta.url)),
-      '!': fileURLToPath(new URL('./vendor', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  let https = false;
+
+  if (mode === 'development') {
+    https = {
+      cert: readFileSync('/etc/certs/cert.pem'),
+      key: readFileSync('/etc/certs/key.pem'),
+    };
+  }
+
+  return {
+    server: {
+      host: 'hub.lan',
+      https,
+      port: 5173,
+      strictPort: true,
+      hmr: { host: 'hub.lan' },
     },
-  },
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: [...refreshPaths, 'resources/**', 'src/**'],
-    }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'script-defer',
-      outDir: 'public/build',
-      base: 'public',
-      buildBase: '/build/',
-      scope: '/',
-      workbox: {
-        cleanupOutdatedCaches: true,
-        directoryIndex: null,
-        globPatterns: ['**/*.{js,css,html,svg,jpg,png,ico,txt,woff,woff2}'],
-        maximumFileSizeToCacheInBytes: 4194304,
-        navigateFallback: null,
-        navigateFallbackDenylist: [/\/[api,admin,livewire]+\/.*/],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./', import.meta.url)),
+        '~': fileURLToPath(new URL('./node_modules', import.meta.url)),
+        '!': fileURLToPath(new URL('./vendor', import.meta.url)),
       },
-      manifest: {
-        name: 'Foxws',
-        short_name: 'Foxws',
-        description: 'Foxws',
-        theme_color: '#030712',
-        background_color: '#030712',
-        orientation: 'portrait-primary',
-        id: '/',
+    },
+    plugins: [
+      laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: [...refreshPaths, 'resources/**', 'src/**'],
+      }),
+      VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: 'script-defer',
+        outDir: 'public/build',
+        base: 'public',
+        buildBase: '/build/',
         scope: '/',
-        start_url: '/',
-        icons: [
-          {
-            src: '/storage/images/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+        workbox: {
+          cleanupOutdatedCaches: true,
+          directoryIndex: null,
+          globPatterns: ['**/*.{js,css,html,svg,jpg,png,ico,txt,woff,woff2}'],
+          maximumFileSizeToCacheInBytes: 4194304,
+          navigateFallback: null,
+          navigateFallbackDenylist: [/\/[api,admin,livewire,vod]+\/.*/],
+        },
+        manifest: {
+          name: 'Hub',
+          short_name: 'Hub',
+          description: 'Hub',
+          theme_color: '#030712',
+          background_color: '#030712',
+          orientation: 'portrait-primary',
+          id: '/',
+          scope: '/',
+          start_url: '/',
+          icons: [
+            {
+              src: '/storage/images/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/storage/images/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+    ],
+    build: {
+      chunkSizeWarningLimit: 1024,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            utils: ['axios'],
+            ws: ['laravel-echo', 'pusher-js'],
           },
-          {
-            src: '/storage/images/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-  ],
-  build: {
-    chunkSizeWarningLimit: 1024,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          http: ['axios'],
-          ws: ['laravel-echo', 'pusher-js'],
         },
       },
     },
-  },
-}));
+  };
+});

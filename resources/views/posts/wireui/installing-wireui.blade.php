0cb71ee1c782875c473ef8cc37b41c512edda3ce
francoism90 <francoism90@users.noreply.github.com>
2024-04-16 15:46:25 +0200

    Add Tailwind

--- resources/views/posts/wireui/installing-wireui.blade.php
+++ resources/views/posts/wireui/installing-wireui.blade.php
@@ -19,3 +19,26 @@ Optionally, you can publish the views using:
 ```bash
 php artisan vendor:publish --tag="wireui-views"
 ```
+
+### Tailwind
+
+When using [Tailwind CSS](https://tailwindcss.com/), make sure to register the templates in `tailwind.config.js`:
+
+```js
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import preset from './vendor/foxws/wireui/resources/css/presets/tailwind.config.preset';
+import theme from './resources/support/tailwind.config.preset';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  presets: [preset, theme],
+  content: [
+    './resources/views/**/*.blade.php',
+    './src/App/**/*.blade.php',
+    './vendor/foxws/wireui/**/*.blade.php',
+  ],
+  plugins: [forms, typography],
+};
+```
+

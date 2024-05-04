3be802a03a316807e2ac1d8b6004804fc80432f7
francoism90 <francoism90@users.noreply.github.com>
2024-05-04 11:45:10 +0200

    Expand about

--- resources/views/landing/about.blade.php
+++ resources/views/landing/about.blade.php
@@ -1,12 +1,13 @@
 <x-wireui::layout-container>
     <main class="flex flex-col py-6 gap-y-6 prose prose-invert max-w-none prose-headings:my-0 prose-h1:font-semibold prose-h1:text-3xl prose-p:my-1 prose-thead:text-left prose-a:text-primary-300">
         <section>
-            <h1>{{ __('About Foxws') }}</h1>
+            <h1>About Foxws</h1>
             <p>
-                {{ __('Foxws is a side project of François Menning.') }}
+                Foxws is a side project of <a href="https://www.linkedin.com/in/francoismenning/">François Menning</a>.
+                It's nothing commercial, just to develop myself as a developer.
             </p>
             <p>
-                {{ __("I'm mainly active over at GitHub, or busy with work.") }}
+                I'm mainly active over at <a href="https://github.com/francoism90/">GitHub</a>, watch sports on various streaming services, or busy with work.
             </p>
         </section>
     </main>

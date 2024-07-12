b6700db0caf11bf4f908e2e216811c0312978394
francoism90 <francoism90@users.noreply.github.com>
2024-07-12 10:34:25 +0200

    wip

--- resources/views/posts/wireuse/introduction-to-wireuse.blade.php
+++ resources/views/posts/wireuse/introduction-to-wireuse.blade.php
@@ -1,11 +1,11 @@
 ## What is WireUse?
 
-WireUse is a collection of helpful utilities to Livewire V3.
+WireUse is a collection of helpful utilities for Livewire V3.
 
 The project tries to rely as much as possible on the functionalities that both Laravel and Livewire already offer,
-and to offer useful features on top of that.
+and offer useful features and utilities on top of that.
 
-The goal is to be minimal, and to give you as a developer tools to build your own components.
+The goal is to be minimal, and to give you as a developer tools to build your own set of Livewire and Blade components.
 
 ## Can I combine WireUse with other packages?
 
@@ -14,9 +14,4 @@ It can be used as an extension in your own project, but also in your own package
 
 ## Who created WireUse?
 
-WireUse was initially created by [François Menning](https://www.linkedin.com/in/francoismenning/), a developer from the Netherlands.
-
-## Why the name WireUse?
-
-The package was originally going to be called LivewireUse.
-To avoid trademark issues, and also to provide a shorter name, a friendly colleague [Amir Swaleh](https://github.com/Amiejah) came up with the name WireUse.
+WireUse is created by [François Menning](https://www.linkedin.com/in/francoismenning/), a developer from the Netherlands.

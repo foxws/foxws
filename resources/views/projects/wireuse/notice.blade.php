8f2af18d504de4569b0fe6ae3e6d093a9b5f352c
francoism90 <francoism90@users.noreply.github.com>
2024-07-12 11:14:58 +0200

    wip

--- resources/views/projects/wireuse/notice.blade.php
+++ resources/views/projects/wireuse/notice.blade.php
@@ -1,3 +1,3 @@
 <x-markdown>
-> Note: The documentation is far from complete, please see [this discussion](https://github.com/foxws/wireuse/discussions/3) for progress.
+> Note: The documentation always follows the latest release (v2.x).
 </x-markdown>

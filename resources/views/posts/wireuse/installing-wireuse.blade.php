ca9e4fe82f357f08fe6f715f11fb17186562ea6d
francoism90 <francoism90@users.noreply.github.com>
2024-07-12 10:36:51 +0200

    wip

--- resources/views/posts/wireuse/installing-wireuse.blade.php
+++ resources/views/posts/wireuse/installing-wireuse.blade.php
@@ -13,34 +13,3 @@ Optionally, you can publish the config file with:
 ```bash
 php artisan vendor:publish --tag="wireuse-config"
 ```
-
-Optionally, you can publish the views using:
-
-```bash
-php artisan vendor:publish --tag="wireuse-views"
-```
-
-> NOTE: It is recommended to build your own views or extend views instead.
-
-### Tailwind
-
-In order to render and build Tailwind CSS classes, you also need to include the vendor paths in your `tailwind.config.js`:
-
-```js
-import forms from "@tailwindcss/forms";
-import typography from "@tailwindcss/typography";
-import preset from "./vendor/foxws/wireuse/resources/css/presets/tailwind.config.preset";
-import theme from "./resources/support/tailwind.config.preset";
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  presets: [preset, theme],
-  content: [
-    "./resources/**/*.blade.php",
-    "./src/**/*.php",
-    "./vendor/foxws/wireuse/**/*.php",
-    "./vendor/foxws/wireuse/**/*.blade.php",
-  ],
-  plugins: [forms, typography],
-};
-```

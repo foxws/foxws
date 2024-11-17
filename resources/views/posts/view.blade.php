184ae6c19657054d7e85e7a9174d8934b0afdc71
francoism90 <francoism90@users.noreply.github.com>
2024-11-17 22:54:27 +0100

    wip

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -23,14 +23,15 @@
         </x-markdown>
     {{ html()->div()->close() }}
 
-    {{ html()->div()->class('pt-6 flex flex-wrap justify-between gap-3 border-t border-primary-700/80')
-        ->childIf($this->previous, html()->a()->class('btn btn-outlined')->link('posts.view', ['project' => $post->project, 'post' => $this->previous ?? $post])->children([
-            html()->icon()->svg('heroicon-o-chevron-left', 'size-4'),
-            html()->span()->text($this->previous?->name)
-        ]))
-        ->childIf($this->next, html()->a()->class('btn btn-outlined')->link('posts.view', ['project' => $post->project, 'post' => $this->next ?? $post])->children([
-            html()->icon()->svg('heroicon-o-chevron-right', 'size-4'),
-            html()->span()->text($this->next?->name)
-        ]))
-    }}
+    {{ html()->div()->class('pt-6 navbar gap-3 border-t border-primary-700/80')->children([
+        html()->div()->class('navbar-center flex-wrap gap-3')
+            ->childIf($this->previous, html()->a()->class('btn btn-outlined')->link('posts.view', ['project' => $post->project, 'post' => $this->previous ?? $post])->children([
+                html()->icon()->svg('heroicon-o-chevron-left', 'size-3.5'),
+                html()->span()->text($this->previous?->name)
+            ]))
+            ->childIf($this->next, html()->a()->class('btn btn-outlined')->link('posts.view', ['project' => $post->project, 'post' => $this->next ?? $post])->children([
+                html()->icon()->svg('heroicon-o-chevron-right', 'size-3.5'),
+                html()->span()->text($this->next?->name)
+        ])),
+    ]) }}
 {{ html()->div()->close() }}

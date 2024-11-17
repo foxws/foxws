a0126e6bee58bad7d75e4c6bbfaab1d115adc015
francoism90 <francoism90@users.noreply.github.com>
2024-11-17 21:55:33 +0100

    wip

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -1,9 +1,9 @@
 {{ html()->div()->class('page')->children([
-    html()->div()->class('page-content prose-h1:text-3xl')->children([
-        html()->div()->children([
+    html()->div()->class('page-content')->children([
+        html()->div()->class('prose-h1:mb-0 prose-h1:text-3xl')->children([
             html()->element('h1')->text($post->name),
 
-            html()->element('dl')->class('dl')
+            html()->element('dl')->class('not-prose divider')
                 ->childrenIf($post->project, [
                     html()->element('dt')->text('Project')->class('sr-only'),
                     html()->element('dd')->child(html()->a()->link('projects.view', $post->project)->text($post->project->name)),

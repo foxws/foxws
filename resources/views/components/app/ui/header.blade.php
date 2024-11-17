cc1df86933c54bd6b22734b02fe589e95ad80374
francoism90 <francoism90@users.noreply.github.com>
2024-11-17 19:44:32 +0100

    feat: add configuration for heroicons and implement navbar styles

--- resources/views/components/app/ui/header.blade.php
+++ resources/views/components/app/ui/header.blade.php
@@ -1,19 +1,19 @@
 {{ html()->element('header')->class('h-14 min-h-14 w-full border-b border-primary-800/80')->children([
-    html()->element('nav')->class('container h-full flex flex-nowrap items-stretch justify-between gap-x-3 overflow-x-auto sm:gap-x-12')->children([
-        html()->div()->class('inline-flex w-2/4 items-center justify-start')->child(
+    html()->element('nav')->class('navbar container h-full')->children([
+        html()->div()->class('navbar-start')->child(
             html()->a()->link('home')->child(
                 html()->span('Foxws')->class('font-semibold text-2xl text-primary-400 hover:text-primary-300')
             )
         ),
 
-        // html()->div()->class('*:inline-flex *:h-full *:gap-1 *:py-0.5 *:text-sm *:font-medium *:line-clamp inline-flex w-2/4 items-center justify-end gap-x-3')->children([
-        //     html()->a()->link('search.index')->attribute('aria-label', __('Search'))->child(
-        //         html()->icon()->svg('heroicon-o-magnifying-glass', 'size-6 text-inherit'),
-        //     ),
+        html()->div()->class('navbar-end gap-x-6 *:text-lg *:text-primary-300 *:hover:text-primary-300')->children([
+            html()->a()->link('home')->child(
+                html()->span('About')->class('text-primary-300 hover:text-primary-300')
+            ),
 
-        //     html()->a()->link('account.notifications')->attribute('aria-label', __('Notifications'))->child(
-        //         html()->icon()->svg('heroicon-o-bell', 'size-6 text-inherit'),
-        //     ),
-        // ]),
+            html()->a()->link('home')->attribute('aria-label', __('Search'))->child(
+                html()->icon()->svg('heroicon-o-magnifying-glass', 'size-6 text-inherit'),
+            ),
+        ]),
     ])
 ]) }}

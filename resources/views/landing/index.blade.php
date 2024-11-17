9bda9040378eba66bed93e2c1d1b779d09eeed89
francoism90 <francoism90@users.noreply.github.com>
2024-11-17 20:54:25 +0100

    wip

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -1,30 +1,42 @@
 @use('Domain\Projects\Models\Project')
 
-{{ html()->div()->class('container py-6')->children([
-    html()->div()->class('prose')->children([
-        html()->element('h1')->text('Current Projects'),
-        html()->element('p')->text('The following projects will receive updates and are documented:'),
+{{ html()->div()->class('page')->children([
+    html()->div()->class('page-content')->children([
+        html()->div()->children([
+            html()->element('h1')->text('Current Projects'),
+            html()->element('p')->text('The following projects will receive updates and are documented:'),
 
-        html()->element('table')->class('table-auto')->children([
-            html()->element('thead')->child(
-                html()->element('tr')->children([
-                    html()->element('th')->text('Name'),
-                    html()->element('th')->text('Description'),
-                ]),
-            ),
+            html()->element('table')->class('table-auto')->children([
+                html()->element('thead')->child(
+                    html()->element('tr')->children([
+                        html()->element('th')->text('Name'),
+                        html()->element('th')->text('Description'),
+                    ]),
+                ),
 
-            html()->element('tbody')->children($this->items, fn (Project $item) => html()
-                ->element('tr')
-                ->children([
-                    html()->element('td')->child(
-                        html()->element('a')
-                            // ->href(route('projects.show', $item))
-                            ->text($item->name),
-                    ),
+                html()->element('tbody')->children($this->items, fn (Project $item) => html()
+                    ->element('tr')
+                    ->children([
+                        html()->element('td')->child(
+                            html()->element('a')
+                                // ->href(route('projects.show', $item))
+                                ->text($item->name),
+                        ),
 
-                    html()->element('td')->text($item->description),
-                ]),
-        ),
+                        html()->element('td')->text($item->description),
+                    ]),
+                ),
+            ])
+        ]),
+
+        html()->div()->children([
+            html()->element('h2')->text('About Foxws'),
+            html()->element('p')->text('Foxws is a side-project to showcase some of my projects and offer documentation for them when needed.'),
+        ]),
+
+        html()->div()->children([
+            html()->element('h2')->text('Get in touch'),
+            html()->element('p')->html('If you would like to send me a message, you can do so on <a href="https://github.com/francoism90/">GitHub</a> or my <a href="https://www.linkedin.com/in/francoismenning/">LinkedIn</a>.'),
         ]),
     ]),
 ]) }}

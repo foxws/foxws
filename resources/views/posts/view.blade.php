5e007d46123a8da3a275d4da98351935ecb4f17c
francoism90 <francoism90@users.noreply.github.com>
2024-11-18 13:20:36 +0100

    wip

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -7,13 +7,9 @@
                     html()->element('dt')->text('Project')->class('sr-only'),
                     html()->element('dd')->child(html()->a()->link('projects.view', $post->project)->text($post->project->name)),
                 ])
-                // ->childrenIf($post->category, [
-                //     html()->element('dt')->text('Category')->class('sr-only'),
-                //     html()->element('dd')->text($post->category),
-                // ])
                 ->children([
                     html()->element('dt')->text('Updated')->class('sr-only'),
-                    html()->element('dd')->text($post->diff_updated),
+                    html()->element('dd')->text("Updated {$post->diff_updated}"),
                 ])
         ]) }}
 

c3ee21e93a81409776bb1736f651a6a62263bf95
francoism90 <francoism90@users.noreply.github.com>
2024-11-18 22:39:04 +0100

    wip

--- routes/auth.php
+++ routes/auth.php
@@ -2,8 +2,5 @@
 
 declare(strict_types=1);
 
-use App\Web\Auth\Controllers\LoginController;
-use App\Web\Auth\Controllers\LogoutController;
-
-// Route::get('/login', LoginController::class)->middleware('guest')->name('login');
-// Route::get('/logout', LogoutController::class)->name('logout');
+Route::get('/login', fn () => abort(404))->name('login');
+Route::get('/logout', fn () => abort(404))->name('logout');
